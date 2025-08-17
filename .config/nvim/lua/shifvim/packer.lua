1e99f3b132a9884d0f04d354e92c8946d0813d32
kevin smith <kev@localhost.localdomain>
2025-08-17 15:36:17 -0700

    colors

--- .config/nvim/lua/shifvim/packer.lua
+++ .config/nvim/lua/shifvim/packer.lua
@@ -11,5 +11,13 @@ return require('packer').startup(function(use)
 	   -- or                            , branch = '0.1.x',
 	   requires = { {'nvim-lua/plenary.nvim'} }
     }
-   -- test
+  use ({
+	'rose-pine/neovim',
+	as = 'rose-pine',
+	config = function()
+		vim.cmd('colorscheme rose-pine')
+	end
+  })
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
 end)
