a3725fd5dc4cff3f9072be3aa98b9cff15f162fb
shifkey <sevensidedsilver@gmail.com>
2025-08-21 12:48:51 -0700

    zsh

--- script/shake.zsh
+++ script/shake.zsh
@@ -7,6 +7,6 @@ echo $drives | jq '.[][0]?'
 is_rm=$drives | jq '.[][0].rm?'
 size_check=$drives | jq '.[][0].size?'
 
-if [[ is_rm ]]; then
+if [[ is_rm && ( size_check -lt 31 ) ]]; then
     echo "its removable"
-    
+fi
