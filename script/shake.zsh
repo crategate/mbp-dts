025e3d094d1591476979cf4b1a31974945b10ea6
shifkey <sevensidedsilver@gmail.com>
2025-08-21 09:55:42 -0700

    shake script

--- script/shake.zsh
+++ script/shake.zsh
@@ -4,3 +4,9 @@
 drives=$(lsblk --json) &> /dev/null
 
 echo $drives | jq '.[][0]?'
+is_rm=$drives | jq '.[][0].rm?'
+size_check=$drives | jq '.[][0].size?'
+
+if [[ is_rm ]]; then
+    echo "its removable"
+    
