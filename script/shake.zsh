7dcb168b216823c35dfed13b6df1f5036efa9eaf
shifkey <sevensidedsilver@gmail.com>
2025-08-22 10:49:04 -0700

    brackets break idk

--- script/shake.zsh
+++ script/shake.zsh
@@ -4,9 +4,23 @@
 drives=$(lsblk --json) &> /dev/null
 
 echo $drives | jq '.[][0]?'
-is_rm=$drives | jq '.[][0].rm?'
+
+
+is_rm=$(echo $drives | jq '.[][0].rm?')
+sd_name=$(echo $drives | jq '.[][0].name?')
 size_check=$drives | jq '.[][0].size?'
 
-if [[ is_rm && ( size_check -lt 31 ) ]]; then
+echo $drives | jq '.[][0].rm?'
+
+echo $is_rm
+echo $sd_name
+
+
+
+#if [[ $(echo $is_rm) && ( 2 -lt 21 )]]
+if $is_rm && false
+then
     echo "its removable"
+else 
+    echo "some other thing"
 fi
