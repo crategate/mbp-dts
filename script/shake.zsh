87cbfc9eb449a5d9ad06fa21f650c7abdb71d1bd
shifkey <sevensidedsilver@gmail.com>
2025-08-22 11:27:13 -0700

    mount works

--- script/shake.zsh
+++ script/shake.zsh
@@ -7,20 +7,21 @@ echo $drives | jq '.[][0]?'
 
 
 is_rm=$(echo $drives | jq '.[][0].rm?')
-sd_name=$(echo $drives | jq '.[][0].name?')
-size_check=$drives | jq '.[][0].size?'
+sd_name=$(echo $drives | jq -r '.[][0].name?')
+size=$(echo $drives | jq '.[][0].size?')
 
-echo $drives | jq '.[][0].rm?'
 
 echo $is_rm
 echo $sd_name
+echo $size
 
 
 
 #if [[ $(echo $is_rm) && ( 2 -lt 21 )]]
-if $is_rm && false
+if $is_rm 
 then
     echo "its removable"
+    sudo mount /dev/$sd_name /media/usb
 else 
     echo "some other thing"
 fi
