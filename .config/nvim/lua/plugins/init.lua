abbaf9ff0d6460626c9ac3dd504614cfc7bc54fc
shifkey <sevensidedsilver@gmail.com>
2025-10-15 15:33:52 -0700

    broke the fucking zsh

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -12,16 +12,18 @@ return {
       require "configs.lspconfig"
     end,
   },
-  {
-	"Pocco81/auto-save.nvim",
-	config = function()
-		 require("auto-save").setup {
-			-- your config goes here
-			-- or just leave it empty :)
-		 }
-	end,
+{
+  "okuuva/auto-save.nvim",
+  version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+  cmd = "ASToggle", -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  opts = {
+    -- your config goes here
+    -- or just leave it empty :)
   },
-  {
+},
+
+ {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
